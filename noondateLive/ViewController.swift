ecd38ee29d519d1afb12f5d66ef7249e50e9882c
윤대식 <t29desmond@gmail.com>
2022-05-23 01:06:35 +0900

    first commit

---
+++ noondateLive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  noondateLive
+//
+//  Created by 윤대식 on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
